c0c4717196fffd16b0c4a6a2006c44ce24cf34bd
MusaRikhotso <1090294@students.wits.ac.za>
2018-08-29 03:06:16 +0200

    finished sign up back-end

--- app/src/main/java/com/example/mid_year_registration/SignUpActivity.java
+++ app/src/main/java/com/example/mid_year_registration/SignUpActivity.java
@@ -1,98 +1,94 @@
 package com.example.mid_year_registration;
 
-import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    @Override
+    EditText e1, e2;
+    Button button;
+    CheckBox checkBox;
+
+    String userName, userPass;
+    String checkAdminPrev;
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup_activity);
-    }
 
-    public void saveToServer(View v){
-        EditText e1 = findViewById(R.id.usernameEditText);
-        EditText e2 = findViewById(R.id.passwordEditText);
-        String userName = e1.getText().toString();
-        String userPass = e2.getText().toString();
-        BackgroundTask b1 = new BackgroundTask();
-        BackgroundTask b2 = new BackgroundTask();
-        b1.execute(userName);
-        b2.execute(userPass);
-    }
+        e1 = findViewById(R.id.usernameEditText);
+        e2 = findViewById(R.id.passwordEditText);
+        button = (Button) findViewById(R.id.submitButton);
+        checkBox = (CheckBox) findViewById(R.id.adminCheckBox);
 
-    @SuppressLint("StaticFieldLeak")
-    class BackgroundTask extends AsyncTask<String,Void,String>
-    {
-        String my_url;
+        userName = e1.getText().toString();
+        userPass = e2.getText().toString();
 
-        @Override
-        protected String doInBackground(String... params) {
+        if(checkBox.isChecked()) {
+            checkAdminPrev = "coordinator";
+        }else{
+            checkAdminPrev = "student";
+        }
 
-            String email_address = params[0];
+        button.setOnClickListener(new View.OnClickListener() {
 
-            try{
-                URL url = new URL(my_url);
-                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoOutput(true);
-
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(outputStream,"UTF-8"));
-
-                String my_data1 = URLEncoder.encode("user_email","UTF-8")+"="+URLEncoder.encode(email_address,"UTF-8");
-
-                bw.write(my_data1);
-                bw.flush();
-                bw.close();
-                outputStream.close();
-
-                InputStream inputStream = httpURLConnection.getInputStream();
-                inputStream.close();
-
-                httpURLConnection.disconnect();
-
-
-            }catch (MalformedURLException e){
-                e.printStackTrace();
-            }catch (IOException e){
-                e.printStackTrace();
+            @Override
+            public void onClick(View v) {
+                userName = e1.getText().toString();
+                userPass = e2.getText().toString();
+                if(checkBox.isChecked()) {
+                    checkAdminPrev = "coordinator";
+                }else{
+                    checkAdminPrev = "student";
+                }
+                new SummaryAsyncTask().execute((Void) null);
             }
-
-
-            return "Data Inserted";
-        }
-
-        @Override
-        protected void onPreExecute() {
-            my_url = "http://lamp.ms.wits.ac.za/~s1153631/signup.php";
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Toast.makeText(getApplicationContext(),result,Toast.LENGTH_LONG).show();
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
+        });
     }
 
+    class SummaryAsyncTask extends AsyncTask<Void, Void, Boolean> {
+
+        private void postData(String mail, String pass, String check) {
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost("http://lamp.ms.wits.ac.za/~s1153631/signup.php");
+
+            try {
+                ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(8);
+                nameValuePairs.add(new BasicNameValuePair("user_email", mail));
+                nameValuePairs.add(new BasicNameValuePair("user_pass", pass));
+                nameValuePairs.add(new BasicNameValuePair("user_type", check));
+
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+            }
+            catch(Exception e)
+            {
+                Log.e("log_tag", "Error:  "+e.toString());
+            }
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            postData(userName, userPass, checkAdminPrev);
+            return null;
+        }
+    }
 }
