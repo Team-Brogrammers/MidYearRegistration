6865a7f35342fbe855ac2479c486bb00a269c9a7
MusaRikhotso <1090294@students.wits.ac.za>
2018-08-30 05:46:52 +0200

    Currently working on logout

--- app/src/main/java/com/example/mid_year_registration/MainActivity.java
+++ app/src/main/java/com/example/mid_year_registration/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mid_year_registration;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
     }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_logout){
+            onLogout();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * This function logs users out the application
+     */
+    private boolean onLogout() {
+        Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+        MainActivity.this.finish();
+        return true;
+    }
+
 }
