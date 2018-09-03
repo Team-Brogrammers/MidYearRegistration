b14495a17f7c2bbad9a4950a0d1e435e45654c7e
MusaRikhotso <1090294@students.wits.ac.za>
2018-09-03 17:19:49 +0200

    Changed user login backend from MySQL server to firebase: user can login successfully

--- app/src/main/java/com/example/mid_year_registration/SignUpActivity.java
+++ app/src/main/java/com/example/mid_year_registration/SignUpActivity.java
@@ -2,14 +2,10 @@ package com.example.mid_year_registration;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -20,33 +16,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import static com.example.mid_year_registration.LoginActivity.CONNECTION_TIMEOUT;
-import static com.example.mid_year_registration.LoginActivity.READ_TIMEOUT;
-
-
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
