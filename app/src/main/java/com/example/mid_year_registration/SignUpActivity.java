d732bd0012b2890008eac70e628df56601df4820
1521128 <1521128@students.wits.ac.za>
2018-09-08 17:23:20 +0200

    I have enhanced the signup email verification by only allowing the users who clicked the verification links sent to thier emails.
    After a user has entered his/her details he needs to pressa a verify button which runs all the checkings and sent him/her a verification link.
    
    SignUp button works if users details are entered and verified by pressing a Verify button

--- app/src/main/java/com/example/mid_year_registration/SignUpActivity.java
+++ app/src/main/java/com/example/mid_year_registration/SignUpActivity.java
@@ -6,18 +6,24 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.lib.ValidateEmail;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+public class SignUpActivity extends AppCompatActivity {
 
     private static EditText e1;
     private static EditText e2;
@@ -28,9 +34,11 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
     static String userPass;
     static String checkAdminPrev;
     static String studentNumber;
+     static FirebaseUser user = null;
+
 
     /*Firebase Libraies*/
-    private FirebaseAuth firebaseAuth;
+    static FirebaseAuth firebaseAuth;
 
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -45,24 +53,17 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         progressDialog = new ProgressDialog(this);
 
         /*Check whether the user is already signed in*/
-        if(firebaseAuth.getCurrentUser() != null){
+        if (firebaseAuth.getCurrentUser() != null) {
             //finish();
             /*Take the user to home*/
         }
-        if(getSupportActionBar() != null){
+        if (getSupportActionBar() != null) {
             //enable back button
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        button.setOnClickListener(this);
-    }
 
-    @Override
-    public void onClick(View v) {
-        if(v == button){
-            registerUser();
-        }
     }
 
     /*Take the user back to the login activity*/
@@ -73,55 +74,84 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
         finish();
     }
 
-    private void registerUser() {
+    public void registerUser(View view) {
         userName = e1.getText().toString().trim();
         userPass = e2.getText().toString().trim();
 
         /*Validate user inputs*/
-        if(TextUtils.isEmpty(userName)){
+        if (TextUtils.isEmpty(userName)) {
             Toast.makeText(getApplicationContext(),
                     "Email cannot be empty",
                     Toast.LENGTH_SHORT).show();
+            //e1.findFocus();
         }
-        if(TextUtils.isEmpty(userPass)){
+        if (TextUtils.isEmpty(userPass)) {
             Toast.makeText(getApplicationContext(),
                     "Password cannot be empty",
                     Toast.LENGTH_SHORT).show();
+            //e2.findFocus();
         }
 
-   /* public boolean onOptionsItemSelected(MenuItem item) {
-
-        if(item.getItemId()==android.R.id.home) {
-            Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }
-        return super.onOptionsItemSelected(item);
-    }*/
-
-        /*Add user information to the database*/
         progressDialog.setMessage("You are being registered...");
         progressDialog.show();
 
-        firebaseAuth.createUserWithEmailAndPassword(userName,userPass)
+        firebaseAuth.createUserWithEmailAndPassword(userName, userPass)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         progressDialog.dismiss();
-                        if(task.isSuccessful()){
+                        if (task.isSuccessful()) {
+
                             /*Successfully Registered*/
+
                             Toast.makeText(getApplicationContext(),
                                     "Registered",
                                     Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-                        }else{
+                            user = FirebaseAuth.getInstance().getCurrentUser();
+                            if (!user.isEmailVerified()) {
+                                user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        Toast.makeText(SignUpActivity.this, "Please click the sent Verification Link to your email", Toast.LENGTH_LONG).show();
+
+                                    }
+                                });
+
+                            }
+
+
+                        } else {
                             Toast.makeText(getApplicationContext(),
-                                    "Ooops! We've encountered a problem.",
+                                    "Ooops! Email Account already in use!.",
                                     Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
     }
+
+
+
+    public void LaunchMainActivity(View view) {
+        FirebaseAuth.getInstance().getCurrentUser().reload();
+        if (user != null) {
+            if (user.isEmailVerified())
+            {
+
+                Toast.makeText(getApplicationContext(),
+                        "Email Verified!",
+                        Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+
+
+
+        } }else {
+            Toast.makeText(getApplicationContext(),
+                    "Fill in the fields and Click Verify button!",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
 
 
