8e3ea5089bb893096ac8c823d59033500d0b88ff
sipheT <1153631@students.wits.ac.za>
2018-09-13 23:02:58 +0200

    Initial tests for student upload

--- app/src/androidTest/java/com/example/mid_year_registration/StudentUploadTest.java
+++ app/src/androidTest/java/com/example/mid_year_registration/StudentUploadTest.java
@@ -1,12 +1,26 @@
 package com.example.mid_year_registration;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.filters.SmallTest;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
+
+import org.junit.Rule;
+import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class StudentUploadTest extends ActivityInstrumentationTestCase2<StudentUpload> {
     StudentUpload activity;
@@ -14,6 +28,9 @@ public class StudentUploadTest extends ActivityInstrumentationTestCase2<StudentU
     public StudentUploadTest() {
         super(StudentUpload.class);
     }
+    @Rule
+    public IntentsTestRule<StudentUpload> intentsTestRule =
+            new IntentsTestRule<>(StudentUpload.class);
 
     @Override
     protected void setUp() throws Exception{
@@ -29,10 +46,53 @@ public class StudentUploadTest extends ActivityInstrumentationTestCase2<StudentU
         onView(withId(R.id.stdNoEditText)).check(matches(isDisplayed()));
         onView(withId(R.id.etCourse)).check(matches(isDisplayed()));
 
-        onView(withId(R.id.btnAddImage)).check(matches(isDisplayed()));
-        onView(withId(R.id.btnConvert)).check(matches(isDisplayed()));
-        onView(withId(R.id.submitButton1)).check(matches(isDisplayed()));
+        onView(withId(R.id.addImageFab)).check(matches(isDisplayed()));
+        onView(withId(R.id.convertImageFab)).check(matches(isDisplayed()));
+        onView(withId(R.id.nextFab)).check(matches(isDisplayed()));
 
+    }
+
+    @SmallTest
+    public void testValidInput(){
+        onView(withId(R.id.stdNoEditText)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.etCourse)).perform(typeText(""), closeSoftKeyboard());
+        onView(withId(R.id.convertImageFab)).perform(click());
+    }
+
+    @SmallTest
+    public void testValidInput1(){
+        onView(withId(R.id.stdNoEditText)).perform(typeText("1234"), closeSoftKeyboard());
+        onView(withId(R.id.etCourse)).perform(typeText("Math3001"), closeSoftKeyboard());
+        onView(withId(R.id.convertImageFab)).perform(click());
+    }
+
+    @SmallTest
+    public void testValidInput2(){
+        onView(withId(R.id.stdNoEditText)).perform(typeText("123467"), closeSoftKeyboard());
+        onView(withId(R.id.etCourse)).perform(typeText("MATH3001"), closeSoftKeyboard());
+        onView(withId(R.id.convertImageFab)).perform(click());
+    }
+
+    @Test
+    public void activityResult_DisplaysContactsPhoneNumber() {
+        // Build the result to return when the activity is launched.
+        Intent resultData = new Intent();
+       // EditText stdNo=;
+        String filename = "1153631_MATH3001";
+        resultData.putExtra("phone", filename);
+        Instrumentation.ActivityResult result =
+                new Instrumentation.ActivityResult(Activity.RESULT_OK, resultData);
+
+        // Set up result stubbing when an intent sent to "contacts" is seen.
+        intending(toPackage("com.android.camera")).respondWith(result);
+
+        // User action that results in "contacts" activity being launched.
+        // Launching activity expects phoneNumber to be returned and displayed.
+        onView(withId(R.id.addImageFab)).perform(click());
+
+        // Assert that the data we set up above is shown.
+        onView(withId(R.id.fileName)).check(matches(withText(filename)));
+        //onView(withId(R.id.fileName)).perform(setName(filename), ;
     }
 
 }
