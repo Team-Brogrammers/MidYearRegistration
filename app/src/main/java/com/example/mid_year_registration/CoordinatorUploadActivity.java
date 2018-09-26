9eb465d9e082ccc5de871954c88b38b24e9db3fa
1521128 <1521128@students.wits.ac.za>
2018-09-26 08:00:43 +0200

    Just did UI Upload PDF Interface

---
+++ app/src/main/java/com/example/mid_year_registration/CoordinatorUploadActivity.java
@@ -0,0 +1,20 @@
+package com.example.mid_year_registration;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CoordinatorUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinator_upload);
+    }
+
+    public void nextPage(View view) {
+    }
+
+    public void openPdf(View view) {
+    }
+}
