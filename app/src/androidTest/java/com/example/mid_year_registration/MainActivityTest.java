78f8854a01130352482bbf327672fe4aed352f7c
FullKickProductions <1240639@students.wits.ac.za>
2018-09-27 23:44:17 +0200

    Add tests for MainActivity

--- app/src/androidTest/java/com/example/mid_year_registration/MainActivityTest.java
+++ app/src/androidTest/java/com/example/mid_year_registration/MainActivityTest.java
@@ -1,44 +1,58 @@
-//package com.example.mid_year_registration;
-//
-//import android.support.test.filters.SmallTest;
-//import android.test.ActivityInstrumentationTestCase2;
-//import android.view.View;
-//
-//import com.google.firebase.database.DatabaseReference;
-//import com.google.firebase.database.FirebaseDatabase;
-//
-//import static android.support.test.espresso.Espresso.onView;
-//import static android.support.test.espresso.action.ViewActions.click;
-//import static android.support.test.espresso.assertion.ViewAssertions.matches;
-//import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
-//import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-//import static android.support.test.espresso.matcher.ViewMatchers.withId;
-//import static android.support.test.espresso.matcher.ViewMatchers.withSubstring;
-//
-//
-//public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
-//    MainActivity activity;
-//    public DatabaseReference databaseReference;
-////    FirebaseDatabase database = FirebaseDatabase.getInstance();
-//    public MainActivityTest() {
-//        super(MainActivity.class);
-////        DatabaseReference.goOffline();
-//    }
-//
-//    @Override
-//    protected void setUp() throws Exception{
-//        super.setUp();
-//        activity = getActivity();
-//    }
-//
-//    @SmallTest
-//    public void testVisibility(){
-//        onView(withId(R.id.recyclerView)).check(matches(hasDescendant(isDisplayed())));
-//    }
+package com.example.mid_year_registration;
+
+import android.support.test.filters.SmallTest;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeDown;
+import static android.support.test.espresso.action.ViewActions.swipeUp;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSubstring;
+
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    MainActivity activity;
+    public MainActivityTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception{
+        super.setUp();
+        activity = getActivity();
+    }
+
+    @SmallTest
+    public void testVisibility(){
+        // dismiss the progress dialog
+        if(activity.getmProgressDialog().isShowing()){
+            activity.getmProgressDialog().dismiss();
+        }
+
+        //check that the recyclerView is visible
+        onView(withId(R.id.recyclerView)).check(matches(isDisplayed()));
+    }
+
+    @SmallTest
+    public void testListScroll(){
+        // dismiss the progress dialog
+        if(activity.getmProgressDialog().isShowing()){
+            activity.getmProgressDialog().dismiss();
+        }
+
+        onView(withId(R.id.recyclerView)).perform(swipeUp());
+        onView(withId(R.id.recyclerView)).perform(swipeDown());
+    }
+
 //
 //    @SmallTest
 //    public void testClick(){
-//        //onView(withId(R.id.recyclerView)).perform(click());
+//        onView(withId(R.id.recyclerView)).perform(click());
 //    }
-//
-//}
+
+}
