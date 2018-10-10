f5dc02b5be0d0772b7ba42594cf5fa229a447b5a
FullKickProductions <1240639@students.wits.ac.za>
2018-10-10 11:37:53 +0200

    Add RecyclerView to StudentConcessionsActivity, populate with concessions.

--- app/src/main/java/com/example/mid_year_registration/StudentConcessionsActivity.java
+++ app/src/main/java/com/example/mid_year_registration/StudentConcessionsActivity.java
@@ -1,29 +1,88 @@
 package com.example.mid_year_registration;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class StudentConcessionsActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference databaseRef;
+    private ProgressDialog mProgressDialog;
+
+    private ArrayList<String> mNames = new ArrayList<>();
+    private ArrayList<String> mImageUrls = new ArrayList<>();
+    private ArrayList<String> mStudentNos = new ArrayList<>();
+    private ArrayList<String> mCourses = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_concessions);
         getSupportActionBar().setTitle("My Requests");
+
         /* Set up the action bar */
         if(getSupportActionBar() != null){
             //enable back button
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+
+        mProgressDialog = new ProgressDialog(StudentConcessionsActivity.this);
+        mProgressDialog.setTitle("Loading Concessions");
+        mProgressDialog.setMessage("Please wait...");
+        mProgressDialog.setCanceledOnTouchOutside(false);
+        mProgressDialog.show();
+
+        databaseRef = database.getReference().child("Concessions");
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // populate the list with concessions
+                for(DataSnapshot childSnap : dataSnapshot.getChildren()){
+                    Concessions concession = childSnap.getValue(Concessions.class);
+                    Log.d("Concession", concession.getPdfUrl());
+                    initImageBitmap(concession.getPdfUrl(), concession.pdfName, concession.studentNo, concession.courseCode);
+                }
+                initRecyclerView();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("DB Error", databaseError.toString()); //TODO handle error properly
+            }
+        });
+    }
+
+    private void initImageBitmap(String url, String name, String studentNo, String course){
+
+        mImageUrls.add(url);
+        mNames.add(name);
+        mStudentNos.add(studentNo);
+        mCourses.add(course);
+    }
+
+    private void initRecyclerView(){
+
+        RecyclerView recyclerView = findViewById(R.id.studentRecyclerView);
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, mNames, mImageUrls, mStudentNos, mCourses);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mProgressDialog.dismiss();
     }
 
     @Override
